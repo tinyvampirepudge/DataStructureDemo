016c7f74577e4ac77c4e64e46b3a8e3cfb7c1fc7
wangjianzhou <879595361@qq.com>
2018-11-01 19:49:37 +0800

    选择排序

--- app/src/main/java/com/tongtong/tiny/datastructureapplication/leetcode/sort/SortAlgorithmActivity.java
+++ app/src/main/java/com/tongtong/tiny/datastructureapplication/leetcode/sort/SortAlgorithmActivity.java
@@ -159,6 +159,40 @@ public class SortAlgorithmActivity extends AppCompatActivity {
      */
     @OnClick(R.id.btn_test3)
     public void onBtnTest3Clicked() {
+        int[] a = generateRandomArray(10, 100);
+        System.out.println(getArrayToString(a));
+        selectSort(a, 10);
+        System.out.println(getArrayToString(a));
+        System.out.println();
+    }
 
+    /**
+     * 插入排序
+     * 从未排序区间中找到最小元素，将其放到已排序区间的末尾。
+     *
+     * @param a
+     * @param n
+     */
+    public void selectSort(int[] a, int n) {
+        if (n <= 1) return;
+        for (int i = 0; i < n - 1; i++) {
+            // 查找最小值的角标
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // 减少无用的交换。
+            if (minIndex == i) {
+                continue;
+            }
+
+            // 交换
+            int tmp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = tmp;
+        }
     }
 }
