8aaafed3221d798bc737855829ee2455877048aa
wangjianzhou <879595361@qq.com>
2018-11-13 17:14:40 +0800

    归并排序

--- app/src/main/java/com/tongtong/tiny/datastructureapplication/leetcode/sort/SortAlgorithmActivity.java
+++ app/src/main/java/com/tongtong/tiny/datastructureapplication/leetcode/sort/SortAlgorithmActivity.java
@@ -14,7 +14,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -31,8 +30,11 @@ import io.reactivex.schedulers.Schedulers;
  * @Version TODO
  */
 public class SortAlgorithmActivity extends AppCompatActivity {
-    @BindView(R.id.tv1)
+    @BindView(R.id.tv_sort_result1)
     TextView tv1;
+
+    @BindView(R.id.tv_sort_result2)
+    TextView tv2;
 
     public static void actionStart(Context context) {
         Intent starter = new Intent(context, SortAlgorithmActivity.class);
@@ -214,7 +216,7 @@ public class SortAlgorithmActivity extends AppCompatActivity {
     @OnClick(R.id.btn_test4)
     public void onBtnTest4Clicked() {
         Observable.create((ObservableOnSubscribe<String>) emitter -> {
-            emitter.onNext(getSortTestResult());
+            emitter.onNext(getSortTestResult1());
             emitter.onComplete();
         })
                 .subscribeOn(Schedulers.computation())
@@ -245,8 +247,8 @@ public class SortAlgorithmActivity extends AppCompatActivity {
 
     }
 
-    private String getSortTestResult() {
-        System.out.println("getSortTestResult Thread.currentThread():" + Thread.currentThread());
+    private String getSortTestResult1() {
+        System.out.println("getSortTestResult1 Thread.currentThread():" + Thread.currentThread());
         int size = 50000;
         int[] a = generateRandomArray(size, 10000);
 
@@ -272,13 +274,102 @@ public class SortAlgorithmActivity extends AppCompatActivity {
         System.out.println();
 
         long startTime3 = System.currentTimeMillis();
-        bubbleSort(a3, size);
+        selectSort(a3, size);
         long endTime3 = System.currentTimeMillis();
-        String result3 = "插入排序开始时间:" + startTime3 + ",结束时间:" + endTime3 + ",总耗时:" + (endTime3 - startTime3);
+        String result3 = "选择排序开始时间:" + startTime3 + ",结束时间:" + endTime3 + ",总耗时:" + (endTime3 - startTime3);
         System.out.println(result3);
         System.out.println();
 
         return "随机数数组大小为:" + size + "\n" + result1 + "\n" + result2 + "\n" + result3;
+    }
+
+    /**
+     * 归并排序
+     */
+    @OnClick(R.id.btn_test5)
+    public void onBtnTest5Clicked() {
+        int size = 10;
+        if (size <= 0) {
+            throw new IllegalArgumentException("size cannot bo lower than 0");
+        }
+        int[] a = generateRandomArray(size, 100);
+        System.out.println("归并排序：");
+        System.out.println(getArrayToString(a));
+        mergeSort(a, size);
+        System.out.println(getArrayToString(a));
+        System.out.println();
+    }
+
+    /**
+     * 归并排序
+     *
+     * @param a 待排序的数组
+     * @param n 数组大小
+     */
+    public void mergeSort(int[] a, int n) {
+        mergeSortRecursion(a, 0, n - 1);
+    }
+
+    /**
+     * 递归调用的函数
+     *
+     * @param a
+     * @param p
+     * @param r
+     */
+    public void mergeSortRecursion(int[] a, int p, int r) {
+        // 递归终止条件
+        if (p >= r) {
+            return;
+        }
+
+        // 取p到r之间的位置q
+        int q = p + (r - p) / 2;
+        // 分治递归
+        mergeSortRecursion(a, p, q);
+        mergeSortRecursion(a, q + 1, r);
+
+        // 将A[p...q]和A[q+1...r]合并为A[p...r]
+        merge(a, p, q, r);
+    }
+
+    /**
+     * 合并两个有序数组
+     *
+     * @param a 合并好的有序数组，需要放到这个位置上。
+     * @param p
+     * @param q
+     * @param r
+     */
+    public void merge(int[] a, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int k = 0;
+        int[] tmp = new int[r - p + 1];
+
+        // 最少把一个数组中的数据取完。
+        while (i <= q && j <= r) {
+            if (a[i] <= a[j]) {
+                tmp[k++] = a[i++];
+            } else {
+                tmp[k++] = a[j++];
+            }
+        }
+
+        // 判断哪个子数组中有剩余的数据。
+        int start = i;
+        int end = q;
+        if (j <= r) {
+            start = j;
+            end = r;
+        }
+        // 将剩余的数据copy到临时数组 tmp。
+        while (start <= end) {
+            tmp[k++] = a[start++];
+        }
+
+        //将 tmp 中的数据拷贝回 a 中
+        System.arraycopy(tmp, 0, a, p, r - p + 1);
     }
 
 }
